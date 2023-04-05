9187c45ce5adbadfdd8c62f33e552948c161a43f
Savenok <Romasav2017@gmail.com>
2023-04-05 17:42:16 +0300

    log

---
+++ log.py
@@ -0,0 +1,11 @@
+#Система лоигрования
+
+
+#Logging system
+
+def wr2(data):
+    with open('logger.txt', 'a') as f:
+        f.write(f'{data}\n')
+def wr(data):
+    with open('logger.txt', 'w') as f:
+        f.write(f'{data}\n')
